f106712d97d6cb9d54960d2fe30c1b736790a203
Zimo Li <7163127+lzm0@users.noreply.github.com>
2022-12-08 20:30:43 -0500

    add plan notes

--- PF/frontend/src/pages/plans/Plans.js
+++ PF/frontend/src/pages/plans/Plans.js
@@ -49,78 +49,84 @@ function Plans() {
   };
 
   return (
-    <div className="hero grow">
-      <div
-        className={
-          "modal modal-bottom sm:modal-middle" +
-          (modalVisibility ? " modal-open" : "")
-        }
-      >
-        {selectedPlan && selectedPlan.id === myPlan.plan ? (
-          <div className="modal-box">
-            <h3 className="font-bold text-lg">
-              Unsubscribe from {selectedPlan?.name.toLowerCase()}
-            </h3>
-            <p>Are you sure you want to unsubscribe from this plan?</p>
-            <div className="modal-action">
-              <button
-                type="button"
-                className="btn btn-ghost"
-                onClick={() => setModalVisibility(false)}
-              >
-                Cancel
-              </button>
-              <button className="btn btn-error" onClick={handlePlanSelect}>
-                Unsubscribe
-              </button>
-            </div>
-          </div>
-        ) : (
-          <div className="modal-box">
-            <h3 className="font-bold text-lg">
-              Subscribe to {selectedPlan?.name.toLowerCase()}
-            </h3>
-            <p>Are you sure you want to subscribe to this plan?</p>
-            <div className="modal-action">
-              <button
-                type="button"
-                className="btn btn-ghost"
-                onClick={() => setModalVisibility(false)}
-              >
-                Cancel
-              </button>
-              <button className="btn btn-primary" onClick={handlePlanSelect}>
-                Subscribe
-              </button>
-            </div>
-          </div>
-        )}
-      </div>
-
-      <div className="flex flex-col hero-content text-center gap-8">
-        <h1 className="text-4xl font-bold">Membership Plans</h1>
-        <div className="stats">
-          {plans.map((plan) => (
-            <div
-              className="stat flex flex-col place-items-center"
-              key={plan.id}
-            >
-              <div className="stat-title">{plan.name}</div>
-              <div className="stat-value">${plan.price}</div>
-              <div
-                className="stat-actions"
-                onClick={() => {
-                  setModalVisibility(true);
-                  setSelectedPlan(plan);
-                }}
-              >
-                <PlanActionButton plan={plan} myPlan={myPlan} />
+    plans && (
+      <div className="hero grow">
+        <div
+          className={
+            "modal modal-bottom sm:modal-middle" +
+            (modalVisibility ? " modal-open" : "")
+          }
+        >
+          {selectedPlan && selectedPlan.id === myPlan.plan ? (
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">
+                Unsubscribe from {selectedPlan?.name.toLowerCase()}
+              </h3>
+              <p>Are you sure you want to unsubscribe from this plan?</p>
+              <div className="modal-action">
+                <button
+                  type="button"
+                  className="btn btn-ghost"
+                  onClick={() => setModalVisibility(false)}
+                >
+                  Cancel
+                </button>
+                <button className="btn btn-error" onClick={handlePlanSelect}>
+                  Unsubscribe
+                </button>
               </div>
             </div>
-          ))}
+          ) : (
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">
+                Subscribe to {selectedPlan?.name.toLowerCase()}
+              </h3>
+              <p>Are you sure you want to subscribe to this plan?</p>
+              <div className="modal-action">
+                <button
+                  type="button"
+                  className="btn btn-ghost"
+                  onClick={() => setModalVisibility(false)}
+                >
+                  Cancel
+                </button>
+                <button className="btn btn-primary" onClick={handlePlanSelect}>
+                  Subscribe
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+
+        <div className="flex flex-col hero-content text-center gap-8">
+          <h1 className="text-4xl font-bold">Membership Plans</h1>
+          <div className="stats">
+            {plans.map((plan) => (
+              <div
+                className="stat flex flex-col place-items-center"
+                key={plan.id}
+              >
+                <div className="stat-title">{plan.name}</div>
+                <div className="stat-value">${plan.price}</div>
+                <div
+                  className="stat-actions"
+                  onClick={() => {
+                    setModalVisibility(true);
+                    setSelectedPlan(plan);
+                  }}
+                >
+                  <PlanActionButton plan={plan} myPlan={myPlan} />
+                </div>
+              </div>
+            ))}
+          </div>
+          <p className="text-sm opacity-60">
+            * All plans are billed immediately after subscription
+            <br /> and can be cancelled at any time.
+          </p>
         </div>
       </div>
-    </div>
+    )
   );
 }
 
