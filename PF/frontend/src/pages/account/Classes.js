cd9466ea713b28c926467b286c67549333a84b67
Zimo Li <7163127+lzm0@users.noreply.github.com>
2022-12-08 20:30:44 -0500

    fix formatting

--- PF/frontend/src/pages/account/Classes.js
+++ PF/frontend/src/pages/account/Classes.js
@@ -47,70 +47,69 @@ function Classes() {
       });
   };
 
-  return classes.length > 0 ? (
+  return (
     <div className="card bg-base-100">
       <div className="card-body">
         <h1 className="card-title">Classes</h1>
-        <div className="overflow-x-auto">
-          <table className="table table-compact w-full">
-            <thead>
-              <tr>
-                <th>Class Date</th>
-                <th>Class Start Time</th>
-                <th>Class End Time</th>
-              </tr>
-            </thead>
-            <tbody>
-              {classes &&
-                classes.map((fitnessClass) => {
-                  const date = new Date(fitnessClass.start_time);
-                  const end = new Date(fitnessClass.end_time);
-                  return (
-                    <tr key={fitnessClass.id}>
-                      <td>
-                        {new Intl.DateTimeFormat("en-US", {
-                          dateStyle: "full",
-                        }).format(date)}
-                      </td>
-                      <td>
-                        {new Intl.DateTimeFormat("en-US", {
-                          timeStyle: "long",
-                        }).format(date)}
-                      </td>
-                      <td>
-                        {new Intl.DateTimeFormat("en-US", {
-                          timeStyle: "long",
-                        }).format(end)}
-                      </td>
-                    </tr>
-                  );
-                })}
-            </tbody>
-          </table>
-        </div>
-        <div className="btn-group grid grid-cols-2 rounded-full overflow-hidden">
-          <button
-            className="btn btn-sm"
-            disabled={!previousPage}
-            onClick={goToPreviousPage}
-          >
-            Previous
-          </button>
-          <button
-            className="btn btn-sm"
-            disabled={!nextPage}
-            onClick={goToNextPage}
-          >
-            Next
-          </button>
-        </div>
-      </div>
-    </div>
-  ) : (
-    <div className="card bg-base-100">
-      <div className="card-body">
-        <h1 className="card-title">Classes</h1>
-        <div className="italic p-8 text-center">No classes enrolled.</div>
+        {classes.length > 0 ? (
+          <div>
+            <div className="overflow-x-auto">
+              <table className="table table-compact w-full">
+                <thead>
+                  <tr>
+                    <th>Date</th>
+                    <th>Start</th>
+                    <th>End</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {classes &&
+                    classes.map((fitnessClass) => {
+                      const date = new Date(fitnessClass.start_time);
+                      const end = new Date(fitnessClass.end_time);
+                      return (
+                        <tr key={fitnessClass.id}>
+                          <td>
+                            {new Intl.DateTimeFormat("en-CA", {
+                              dateStyle: "full",
+                            }).format(date)}
+                          </td>
+                          <td>
+                            {new Intl.DateTimeFormat("en-CA", {
+                              timeStyle: "short",
+                            }).format(date)}
+                          </td>
+                          <td>
+                            {new Intl.DateTimeFormat("en-CA", {
+                              timeStyle: "short",
+                            }).format(end)}
+                          </td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </table>
+            </div>
+            <div className="btn-group grid grid-cols-2 rounded-full overflow-hidden">
+              <button
+                className="btn btn-sm"
+                disabled={!previousPage}
+                onClick={goToPreviousPage}
+              >
+                Previous
+              </button>
+              <button
+                className="btn btn-sm"
+                disabled={!nextPage}
+                onClick={goToNextPage}
+              >
+                Next
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div className="italic p-8 text-center">No classes enrolled.</div>
+        )}
       </div>
     </div>
   );
